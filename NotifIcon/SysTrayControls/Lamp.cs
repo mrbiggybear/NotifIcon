ebad9598a84b8cfda928b9edfe43ef9b40634966
mrbiggybear <landis.bj@gmail.com>
2021-01-14 20:20:56 -0600

    Adding NofiIcon Project

---
+++ NotifIcon/SysTrayControls/Lamp.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SysTrayControls
+//{
+//    public class Lamp
+//    {
+//        public bool state { get; set; }
+
+//        public bool Toggle()
+//        {
+//            state = !state;
+//            return state;
+//        }
+//    }
+//}
